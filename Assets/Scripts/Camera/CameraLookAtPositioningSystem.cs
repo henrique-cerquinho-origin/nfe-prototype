cf403bd3011aaa2a9a95a1edbafe7d5406b7684b
Henrique Cerquinho <henrique.cerquinho@origin-games.com>
2025-11-26 16:40:21 -0300

    better camera config serialization

--- Assets/Scripts/Camera/CameraLookAtPositioningSystem.cs
+++ Assets/Scripts/Camera/CameraLookAtPositioningSystem.cs
@@ -1,7 +1,5 @@
-using Input;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.NetCode;
 using Unity.Transforms;
 
--- Assets/Scripts/Camera/CameraReferencesAuthoring.cs
+++ Assets/Scripts/Camera/CameraReferencesAuthoring.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Unity.Cinemachine;
 using Unity.Entities;
@@ -7,21 +8,27 @@ namespace Camera
 {
     public class CameraReferencesAuthoring : MonoBehaviour
     {
-        [SerializeField] public CinemachineCamera AdventureCamera;
-        [SerializeField] public CinemachineCamera AimingCamera;
+        [SerializeField] public List<CameraConfig> Cameras;
     
         public class Baker : Baker<CameraReferencesAuthoring>
         {
             public override void Bake(CameraReferencesAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.None);
-                var cameras = new Dictionary<CameraType, CinemachineCamera>
+                var cameras = new Dictionary<CameraType, CinemachineCamera>();
+                foreach (CameraConfig configs in authoring.Cameras)
                 {
-                    { CameraType.Adventure, authoring.AdventureCamera },
-                    { CameraType.Aiming, authoring.AimingCamera }
-                };
+                    cameras.Add(configs.Type, configs.Camera);
+                }
                 AddComponentObject(entity, new CameraReferencesComponent { Cameras = cameras });
             }
+        }
+
+        [Serializable]
+        public struct CameraConfig
+        {
+            public CameraType Type;
+            public CinemachineCamera Camera;
         }
     }
 }
